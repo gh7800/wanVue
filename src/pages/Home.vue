8fe22dfa47eb212066b0a9b2ec485351aeecb1d2
wangshuai <bjwangs@163.com>
2020-08-06 18:12:32 +0800

    bug

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,48 @@
 <template>
-    <p>home</p>
+    <div>
+        <el-row style="height: 100%;">
+            <el-col style="height: 100%;width: 20%;background-color: #324057;">
+                <el-menu
+                        router
+                        :default-active="defaultActive">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-edit"></i>
+                            <span>导航一</span>
+                        </template>
+                    </el-submenu>
+                    <el-menu-item index="about">首页的</el-menu-item>
+                    <el-menu-item index="main">首页的</el-menu-item>
+                    <el-menu-item>首页的</el-menu-item>
+                    <el-submenu index="5">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <el-menu-item>首页的</el-menu-item>
+                        <el-menu-item>首页的</el-menu-item>
+                        <el-menu-item>首页的</el-menu-item>
+                        <el-menu-item>首页的</el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-col>
+            <el-col style="height: 100%;width:80%; overflow: auto;">
+                <keep-alive>
+                    <router-view></router-view>
+                </keep-alive>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        computed: {
+            defaultActive: function(){
+                return this.$route.path.replace('/', '');
+            }
+        },
+        name: "Home",
     }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../pages/Home'
-//import Home from '../views/Home.vue'
 import Login from '../pages/Login'
 
 Vue.use(VueRouter);
@@ -15,16 +14,26 @@ const routes = [
     {
         path: '/home',
         name: 'Home',
-        component: Home
+        component: Home,
+        children: [
+            {
+                path: '/about',
+                name: 'About',
+                component: () => import('../views/About.vue')
+            }, {
+                path: '/main',
+                component: () => import('../views/Home.vue')
+            }
+        ]
     },
-    {
+    /*{
         path: '/about',
         name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+        component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+    }*/
 
 ];
 
