abdcb1982e2e37ba22b1eee725d2a2db7ae3418d
wangs <wangshuai_wy@126.com>
2020-08-25 22:42:14 +0800

    ..

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,19 +10,29 @@
                             <i class="el-icon-edit"></i>
                             <span>导航一</span>
                         </template>
+                        <el-menu-item>项目1</el-menu-item>
+                        <el-menu-item>项目2</el-menu-item>
+                        <el-menu-item>项目3</el-menu-item>
+                        <el-menu-item>项目4</el-menu-item>
                     </el-submenu>
-                    <el-menu-item index="about">首页的</el-menu-item>
-                    <el-menu-item index="main">首页的</el-menu-item>
-                    <el-menu-item>首页的</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-magic-stick"></i>
+                            <span>导航二</span>
+                        </template>
+                        <el-menu-item index="about">关于</el-menu-item>
+                        <el-menu-item index="main">home</el-menu-item>
+                        <el-menu-item>首页的</el-menu-item>
+                    </el-submenu>
                     <el-submenu index="5">
                         <template slot="title">
                             <i class="el-icon-location"></i>
-                            <span>导航一</span>
+                            <span>导航三</span>
                         </template>
-                        <el-menu-item>首页的</el-menu-item>
-                        <el-menu-item>首页的</el-menu-item>
-                        <el-menu-item>首页的</el-menu-item>
-                        <el-menu-item>首页的</el-menu-item>
+                        <el-menu-item>项目5</el-menu-item>
+                        <el-menu-item>项目6</el-menu-item>
+                        <el-menu-item>项目7</el-menu-item>
+                        <el-menu-item>项目8</el-menu-item>
                     </el-submenu>
                 </el-menu>
             </el-col>
@@ -38,7 +48,7 @@
 <script>
     export default {
         computed: {
-            defaultActive: function(){
+            defaultActive: function () {
                 return this.$route.path.replace('/', '');
             }
         },
