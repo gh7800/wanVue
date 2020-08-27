2df37fdb0d78a52fc9d92ee5e258269b23643b40
wangs <wangshuai_wy@126.com>
2020-08-27 23:08:35 +0800

    table

--- src/pages/UserList.vue
+++ src/pages/UserList.vue
@@ -1,10 +1,44 @@
 <template>
-    <p>userList</p>
+    <el-table style="width: 100%;" height="180" :data="tableData" border="true">
+        <el-table-column style="width: 100px;color: maroon" prop="name" label="名称" />
+        <el-table-column style="width: 100px" prop="age" label="年龄"/>
+        <el-table-column style="width: 100px" prop="gender" label="性别"/>
+        <el-table-column style="width: 100px;" prop="date" label="时间"/>
+        <el-table-column style="width: 100px;" prop="date" label="时间"/>
+        <el-table-column style="width: 100px;" prop="date" label="时间"/>
+        <el-table-column style="width: 100px;" prop="date" label="时间"/>
+    </el-table>
 </template>
 
 <script>
     export default {
-        name: "UserList"
+        name: "UserList",
+        data() {
+            return {
+                    tableData: [
+                        {
+                            name: "名称",
+                            age: 22,
+                            gender: '男',
+                            date: "2020-08-27 22:49"
+                        },
+                        {
+                            name: "名称",
+                            age: 22,
+                            gender: '女',
+                            date: "2020-08-27 22:49"
+                        },
+                        {
+                            name: "名称",
+                            age: 22,
+                            gender: '女',
+                            date: "2020-08-27 22:49"
+                        }
+                    ]
+                }
+            }
+
+
     }
 </script>
 
