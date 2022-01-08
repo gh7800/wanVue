c57be5f42f30a20f55bf71c0c25ad8ca08e89032
wangshuai <bjwangs@163.com>
2022-01-08 11:28:02 +0800

    首页优化

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="main" style="height:100vh;">
-    <el-container style="background: #f2f2f2;width: 100%; height: 100%;padding: 0;align-content: flex-start;"
-                  direction="horizontal">
-      <el-aside display="block" style="width: 13%;height:100%;padding: 0;overflow-y: hidden;">
-        <el-image :src="require('../assets/2.jpg')" style="width: 100%;height: 75px;margin: 0"></el-image>
+  <div class="main" style="height: 100vh">
+    <el-container
+      style="
+        background: #f2f2f2;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        align-content: flex-start;
+      "
+      direction="horizontal"
+    >
+      <el-aside
+        display="block"
+        style="width: auto; height: 100%; padding: 0; overflow-y: hidden"
+        @mouseenter.native="handleOpen"
+        @mouseleave.native="handleClose"
+      >
         <el-menu
-            router
-            background-color="#545c64"
-            active-text-color="#ffd04b"
-            text-color="#fff"
-            style="height:100%;padding: 0">
+          router
+          background-color="#545c64"
+          active-text-color="#ffd04b"
+          text-color="#fff"
+          :collapse="isCollapse"
+          style="height: 100%; padding: 0,width = auto"
+        >
+          <el-image
+            :src="require('../assets/2.jpg')"
+            style="width: auto; height: 75px; margin: 0"
+            v-model="isCollapse"
+          ></el-image>
           <el-submenu index="1">
             <template slot="title" style="padding: 0">
               <i class="el-icon-edit"></i>
@@ -61,19 +80,39 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main style="background-color: white;padding: 0">
+      <el-main style="background-color: white; padding: 0">
         <el-container class="el-container">
-          <el-row style="background-color: gray;width: 100%;height: 75px" type="flex" justify="space-between">
-            <el-row style="height: 100%;" type="flex" align="middle" justify="start">
-              <i class="el-icon-menu" style="padding-left: 10px"></i>
-              <span style="width: 130px;">移动办公系统</span>
+          <el-row
+            style="background-color: gray; width: 100%; height: 75px"
+            type="flex"
+            justify="space-between"
+          >
+            <el-row
+              style="height: 100%"
+              type="flex"
+              align="middle"
+              justify="start"
+            >
+              <el-button @click="collapseStatus">
+                <i class="el-icon-menu" style="padding-left: 10px"></i>
+              </el-button>
+              <span style="width: 130px">移动办公系统</span>
             </el-row>
-            <el-row type="flex" class="row-bg" justify="space-between" align="middle">
+            <el-row
+              type="flex"
+              class="row-bg"
+              justify="space-between"
+              align="middle"
+            >
               <el-row align="middle" justify="center" type="flex">
-                <el-avatar :src="require('../assets/logo.png')"
-                           @click="avatarClick"
+                <el-avatar
+                  :src="require('../assets/logo.png')"
+                  @click="avatarClick"
                 ></el-avatar>
-                <span style="padding-right: 16px;padding-left: 10px;color: white">名称</span>
+                <span
+                  style="padding-right: 16px; padding-left: 10px; color: white"
+                  >名称</span
+                >
               </el-row>
             </el-row>
           </el-row>
@@ -86,25 +125,41 @@
 
 <script>
 // import Hamburger from '@/components/Hamburger'
-import UserList from './UserList.vue'
+import UserList from "./UserList.vue";
 
 export default {
   name: "Home",
   data() {
-
+    return {
+      isCollapse: true,
+    };
   },
   methods: {
     avatarClick() {
-      this.$message.success("OK")
-    }
+      this.$message.success("OK");
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+      true;
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+      false;
+    },
+    setHide() {
+      isCollapse = !isCollapse;
+    },
+    collapseStatus() {
+      this.collapseBtnClick = this.isCollapse;
+      this.isCollapse = !this.isCollapse;
+    },
   },
   comments: {
     // Hamburger
-    UserList
+    UserList,
   },
-}
+};
 </script>
 
 <style scoped>
-
 </style>
