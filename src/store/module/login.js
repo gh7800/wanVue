70134e8cfed1d8e53066f956582aed90bfa9ab76
wangshuai <bjwangs@163.com>
2022-05-24 18:10:45 +0800

    ..

--- src/store/module/login.js
+++ src/store/module/login.js
@@ -21,24 +21,6 @@ const actions = {
                 reject(error)
             })
         })
-
-        // return new Promise((resolve, reject) => {
-        //     login({ username: username.trim(), password: password }).then(response => {
-        //         const { data } = response
-        //         commit('SET_TOKEN', data.token) // name avatar permission abilities
-        //         commit('SET_NAME', data.title)
-        //         commit('SET_AVATAR', data.avatar_url)
-        //         commit('SET_PERMISSION', data.permission)
-        //         commit('SET_ABILITIES', data.abilities)
-        //         commit('SET_USER', data)
-
-        //         setUserinfo(data)
-        //         setToken(data.token)
-        //         resolve(response)
-        //     }).catch(error => {
-        //         reject(error)
-        //     })
-        // })
     },
 
     logout({commit}){
